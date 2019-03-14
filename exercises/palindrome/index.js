8e9b166270e3f29eda44809db269c1e6472b57c4
Unicar <unicar9@gmail.com>
2019-03-14 11:57:31 +1100

    Added palindrome solution

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,17 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    return str.split('').every((char, i) => {
+        return char === str[str.length - i - 1]
+    })
+}
 
 module.exports = palindrome;
+
+
+// function palindrome(str) {
+//     const reversed = str.split('').reduce((rev, char) => char + rev, '')
+
+//     return str === reversed
+// }
