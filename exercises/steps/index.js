4ac39fdd6747b8fbfa860573ac57a14dd69022a8
unicar <unicar9@gmail.com>
2024-09-23 08:51:10 +1000

    Added steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,45 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// function steps(n) {
+//   for (let i = 0; i < n; i++) {
+//     let stair = '';
+//     for (let j = 0; j < n; j++) {
+//       if (j <= i) {
+//         stair += '#';
+//       } else {
+//         stair += ' ';
+//       }
+//     }
+//     console.log(stair);
+//   }
+// }
+
+// function printNumber(n) {
+//   if (n === 0) {
+//     return;
+//   }
+//   console.log(n);
+//   printNumber(n - 1);
+// }
+
+function steps(n, row = 0, stair = '') {
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  if (stair.length <= row) {
+    stair += '#';
+  } else {
+    stair += ' ';
+  }
+
+  steps(n, row, stair);
+}
 
 module.exports = steps;
